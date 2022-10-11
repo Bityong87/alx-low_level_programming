4fa5934b725f6891a0c474c20fd6654cdcaf8f92
Bityong87 <yusufbityong@yahoo.com>
2022-10-11 07:14:17 -0700

    Write a function that concatenates two strings

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,18 @@
+#include <stdlib.h>
 #include "main.h"
  /**
- * malloc_checked - allocates memory using malloc, 
- * exit(98) if it fails
+ * malloc_checked - allocates memory using malloc, exit(98) if it fails
  * @b: size of the memory block to be allocated
  * Return: pointer to the address of the memory block
  */
 void *malloc_checked(unsigned int b)
 {
-	void *block;
+	void *ptr;
 
-	block = malloc(b);
-	if (block == NULL)
+	ptr = malloc(b);
+
+	if (ptr == NULL)
 		exit(98);
-	return (block);
+
+	return (ptr);
 }
